1089a04cd33e079c15848836e875b0d32d34fffc
Munotasir2762 <muntasirpranto2762@gmail.com>
2025-02-09 23:41:36 +0600

    Array in Js

---
+++ script.js
@@ -0,0 +1,14 @@
+let marks = [59, 60, 56, 76, 90];
+
+console.log(marks[2]);
+
+let sum = marks[2]+marks[3];
+
+console.log(sum);
+
+
+
+let man = {name: "Rahim", age: 78, blood: "O+", phone: "+8801XXXXXXXXX", address: "Uttara, Dhaka"};
+
+console.log(man.blood);
+console.log(man.phone);
